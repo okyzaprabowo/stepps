e34a68142afc9f2e73a234a43aad55572aac75cb
Jeki Maulana <jekibus@gmail.com>
2018-11-02 14:59:37 +0700

    Update file from Whatsapp. First commit.

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-# Jonah Berger, Contagious https://www.youtube.com/watch?v=iRMdvhBiOQU test
+# Jonah Berger, Contagious https://www.youtube.com/watch?v=iRMdvhBiOQU
 
 from InstagramAPI import InstagramAPI
 import json
@@ -6,52 +6,65 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from datetime import datetime
 
+# Login
+username="na_ratnaa"
+InstagramAPI = InstagramAPI(username, "ratnasanti123")
+InstagramAPI.login()
+
 # create stemmer
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
-# Stopword Removal
-stop_factory = StopWordRemoverFactory()
-stopword = stop_factory.create_stop_word_remover()
-data = stop_factory.get_stop_words()
+# # Abaikan ini pekerjaan saya, belum beres
+# s = 'Aku pernah mendengar Aisya bercerita bahwa sebenarnya ia tidak terlalu senang dengan kabar perjodohan yang diatur oleh orang tuanya.'
+ 
+# #Create factory
+# stop_factory = StopWordRemoverFactory()
+ 
+# # Tambahkan Stopword Baru
+# data = stop_factory.get_stop_words() #+more_stopword
+ 
+# stopword = stop_factory.create_stop_word_remover()
+# print(s)
+# print(stopword.remove(s))
 # print(data)
 
-# stem example
-a = "tolongin"
-print(stemmer.stem(a))
+# # # stopwords remove
+# # stop_factory = StopWordRemoverFactory()
+# # more_stopword = ['dengan', 'ia','bahwa','oleh', 'aja', 'nih', 'yg', 'nge']
+# # data = stop_factory.get_stop_words()+more_stopword
+# # stopword = stop_factory.create_stop_word_remover()
 
-
-# Login
-# username="na_ratnaa"
-# InstagramAPI = InstagramAPI(username, "ratnasanti123")
-# InstagramAPI.login()
+# # kalimat = 'Mangga akang" celana jeans nya.. Warna hanya hitam aja.. Size kumplit.. 082217089975'
+# # stop = stopword.remove(kalimat)
+# # print(stop)
 
 # Get personal profile
-# InstagramAPI.getProfileData()
-# result = InstagramAPI.LastJson
-# print(result)
+InstagramAPI.getProfileData()
+result = InstagramAPI.LastJson
+print(result)
 
 # Get from timeline
-# InstagramAPI.timelineFeed()
-# result = InstagramAPI.LastJson
-# feeds = result['items']
+InstagramAPI.timelineFeed()
+result = InstagramAPI.LastJson
+feeds = result['items']
 
-# for x in range(len(feeds)):
-# 	print("----START-----")
-# 	try:
-# 		sentence = feeds[x]['caption']['text']
-# 		timestamp = datetime.utcfromtimestamp(feeds[x]['caption']['created_at']).strftime('%Y-%m-%d %H:%M:%S')
-# 		like_count = feeds[x]['like_count']
-# 		print("Caption -> ", sentence)
-# 		print("Timestamp -> ", timestamp)
-# 		print("Like Count -> ", like_count)
-# 		output = stemmer.stem(sentence)
-# 		print("Stemming -> ", stopword.remove(output))
-# 	except Exception as e:
-# 		print("Tidak ada text") 
-# 	else:
-# 		pass
-# 	finally:
-# 		pass
-# 	# print(feeds[x])
-# 	print("----END-----\n")
+for x in range(len(feeds)):
+	print("----START-----")
+	try:
+		sentence = feeds[x]['caption']['text']
+		timestamp = datetime.utcfromtimestamp(feeds[x]['caption']['created_at']).strftime('%Y-%m-%d %H:%M:%S')
+		like_count = feeds[x]['like_count']
+		print("Caption -> ", sentence)
+		print("Timestamp -> ", timestamp)
+		print("Like Count -> ", like_count)
+		output = stemmer.stem(sentence)
+		print("Stemming -> ", output)
+	except Exception as e:
+		print("Tidak ada text") 
+	else:
+		pass
+	finally:
+		pass
+	# print(feeds[x])
+	print("----END-----\n")
