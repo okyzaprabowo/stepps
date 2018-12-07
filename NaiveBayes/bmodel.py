9dc3fbf2b37afed8ce728aca932ffa4afa1b344d
okyzaprabowo <thesmartmotion@gmail.com>
2018-12-07 21:11:49 +0700

    predict using LinearSVC (59%-65%)

--- NaiveBayes/bmodel.py
+++ NaiveBayes/bmodel.py
@@ -1,11 +1,9 @@
-# from sklearn.metrics import accuracy_score, precision_score, classification_report, confusion_matrix
-# print "Accuracy:", accuracy_score(y_test, preds)
-# print "Precision:", precision_score(y_test, preds)
-# print classification_report(y_test, preds)
-# print confusion_matrix(y_test, preds)
-
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, precision_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
@@ -32,7 +30,7 @@ x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
 train_x, test_x, train_y, test_y = train_test_split(x_train_tfidf, varietal_list, test_size=0.3)
 
-clf = MultinomialNB().fit(train_x, train_y)
+clf = LinearSVC().fit(train_x, train_y)
 y_score = clf.predict(test_x)
 
 n_right = 0
@@ -40,8 +38,12 @@ for i in range(len(y_score)):
     if y_score[i] == test_y[i]:
         n_right += 1
 
-# print("Accuracy: %.2f%%" % ((n_right/float(len(test_y)) * 100)))
 print("Accuracy:", accuracy_score(test_y, y_score))
 # print("Precision:", precision_score(test_y, y_score, average=None))
 # print(classification_report(test_y, y_score))
 # print(confusion_matrix(test_y, y_score))
+
+# array prediction
+print(clf.predict(count_vect.transform(["ceremony", "yayoi kusuma"])))
+
+
